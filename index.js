8858f5b5a4e85e91e5347bc600d154a45ab32e09
Sharoze Archer <sharoze.archer@gmail.com>
2023-03-20 17:56:58 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+
+    return Math.abs(location - 42);
+  }
+
+function distanceFromHqInFeet(location) {
+    const distanceBlocks = distanceFromHqInBlocks(location);
+    return distanceBlocks * 264;
+   }
+
+function distanceTravelledInFeet(start, destination) {
+
+    const distanceTravelled = Math.abs(destination - start);
+    return distanceTravelled * 264;
+  }
+
+function calculatesFarePrice(start, destination) {
+    
+    const disInfeet = distanceTravelledInFeet(start, destination);
+    if (disInfeet <= 400) {
+        return 0;
+    }
+    else if (disInfeet <= 2000) {
+        return (disInfeet - 400) * 0.02;
+    }
+    else if (disInfeet <= 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+  }
